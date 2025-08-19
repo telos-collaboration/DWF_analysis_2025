e3421a9c27795ea239b304db25a08476dd5a604b
Ed Bennett <e.j.bennett@swansea.ac.uk>
2025-08-19 17:20:27 +0100

    be able to specify p0 when bootstrapping fits, and tolerate failed samples

--- src/plot_mres_scans.py
+++ src/plot_mres_scans.py
@@ -183,20 +183,34 @@ def get_rng(data):
     return np.random.default_rng(seed)
 
 
-def bootstrap_curve_fit(fit_form, x, y, sigma):
-    popt, _ = curve_fit(fit_form, x, y, sigma=sigma)
+def bootstrap_curve_fit(fit_form, x, y, sigma, p0=None):
+    popt, _ = curve_fit(fit_form, x, y, sigma=sigma, p0=p0)
     popt_samples = []
     rng = get_rng([x, y, sigma])
-    for _ in range(NUM_BOOTSTRAP_SAMPLES):
+    max_attempts = NUM_BOOTSTRAP_SAMPLES * 5
+    for attempt_count in range(max_attempts):
         sample = rng.choice(x.index, size=len(x))
-        popt_sample, _ = curve_fit(
-            fit_form,
-            x[sample],
-            y[sample],
-            sigma=sigma[sample],
-            p0=popt,
-        )
+        try:
+            popt_sample, _ = curve_fit(
+                fit_form,
+                x[sample],
+                y[sample],
+                sigma=sigma[sample],
+                p0=popt,
+            )
+        except RuntimeError:
+            continue
         popt_samples.append(popt_sample)
+        if len(popt_samples) >= NUM_BOOTSTRAP_SAMPLES:
+            break
+    else:
+        raise RuntimeError("Unable to collect enough bootstrap samples")
+
+    if attempt_count > NUM_BOOTSTRAP_SAMPLES:
+        print(
+            "Bootstrapped with "
+            f"{NUM_BOOTSTRAP_SAMPLES / attempt_count * 100}% efficiency"
+        )
 
     return popt, np.std(popt_samples, axis=0)
 
